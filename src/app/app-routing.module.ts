706a9781a7b49fa7daa1e168c58e52fb215c8566
Pranavan Paranthaman <31650351+pranaone@users.noreply.github.com>
2021-11-14 10:21:09 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PostListComponent},
+  {path: 'create', component: PostCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
